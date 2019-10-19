23a3eb152c8362647f8300075741b4b409ab1f80
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-20 00:36:52 +1100

    added feelings, mental illnesses , situation  :)

---
+++ src/db/hardcoded-mental-ill.js
@@ -0,0 +1,54 @@
+export const mentalIllnesses = [
+  {
+    label: "Addiction",
+    value: "addiction"
+  },
+  {
+    label: "Anxiety Disorder",
+    value: "anxiety"
+  },
+  {
+    label: "Attention Deficit / Hyperactivity",
+    value: "adhd"
+  },
+  {
+    label: "Bipolar",
+    value: "bipolar"
+  },
+  {
+    label: "Depressive Disorder",
+    value: "depressive"
+  },
+  {
+    label: "Disruptive, Impulse-Control, and Conduct Disorder",
+    value: "dsm5"
+  },
+  {
+    label: "Dissociative Disorder",
+    value: "dissociative"
+  },
+  {
+    label: "Eating Disorder",
+    value: "eating"
+  },
+  {
+    label: "Gender Identity",
+    value: "gender"
+  },
+  {
+    label: "Obsessive Compulsive Disorders",
+    value: "ocd"
+  },
+  {
+    label: "Personality Disorder",
+    value: "personality"
+  },
+  {
+    label: "Schizophrenia or Psychosis",
+    value: "psychosis"
+  },
+  {
+    label: "Don't have a clinical diganosis ",
+    value: "unlabeled"
+  }
+];
---
+++ src/db/hardcoded-situations.js
@@ -0,0 +1,66 @@
+export const situations = [
+  {
+    label: "Insecure",
+    value: "insecure"
+  },
+  {
+    label: "Bullying",
+    value: "bullying"
+  },
+  {
+    label: "Disability, Illness, Injury",
+    value: "dii"
+  },
+  {
+    label: "School or education issues",
+    value: "education"
+  },
+  {
+    label: "Family/Parenting",
+    value: "family"
+  },
+  {
+    label: "Phobias",
+    value: "phobias"
+  },
+  {
+    label: "Financial",
+    value: "financial"
+  },
+  {
+    label: "Relationship instability",
+    value: "relationship"
+  },
+  {
+    label: "Grieving",
+    value: "grieving"
+  },
+  {
+    label: "Sexual Abuse",
+    value: "sexualabuse"
+  },
+  {
+    label: "War trauma",
+    value: "war"
+  },
+  {
+    label: "Natural disaster trauma",
+    value: "disaster"
+  },
+  {
+    label: "Life transition ",
+    value: "life"
+  },
+  {
+    label: "LGBTI",
+    value: "lgbti"
+  },
+  {
+    label: "Relationship Breakdown",
+    value: "breakup"
+  },
+  {
+    label: "Domestic Violence",
+    value: "domestic"
+  }
+]
