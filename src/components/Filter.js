23f278d89176145a89a5e2dd21d4484d1329f617
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-19 13:43:21 +1100

    hard coded filter options

---
+++ src/components/Filter.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+let filterOptions = [{
+    title: "Mental Health Condition",
+    options: [{
+        label: "Bipolar",
+        value: "bp"
+      },
+      {
+        label: "Anxiety",
+        value: "ax"
+      },
+      {
+        label: "OCD",
+        value: "ocd"
+      }
+    ]
+  },
+  {
+    title: "feelings",
+    options: [{
+        label: "Angry",
+        value: "angry"
+      },
+      {
+        label: "Sad",
+        value: "sad"
+      },
+      {
+        label: "Anxious",
+        value: "anxious"
+      }
+    ]
+  },
+  {
+    title: "situations",
+    options: [{
+        label: "Coming out of closet",
+        value: "comingout"
+      },
+      {
+        label: "Not being able to find a psychologist",
+        value: "cannotseepsychologist"
+      },
+      {
+        label: "Connecting with your parents",
+        value: "connectparent"
+      }
+    ]
+  },
+]
