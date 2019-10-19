85b51042e8ae694a8036c8c64d0403b4434f7408
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-19 17:01:01 +1100

    changed the filter components

--- src/components/filterOption/index.js
+++ src/components/filterOption/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { filterOptions } from '../hardcoded-filter';
+import {useState} from 'react';
 
-const FilterOption = ({ title, options }) => (
-  <div>
-    <label htmlFor="filter-options">{title}</label>
-    <select id="filter-options">
-      {
-        options.map((option, index) => {
-          return (
-            <option key={index} value={option.value}>{option.label}</option>
-          )
-        })
-      }
-    </select>
-  </div>
-);
+const FilterOption = ({ title, options }) => {
+  const [step2, setStep2] = useState({});
+  const handleEvent = async (e, value, title) =>{
+    setStep2({...step2, [value]: e.target.checked})
+  }
+
+  return(
+      <div>
+        <label htmlFor="filter-options">{title}</label>
+        <div id="filter-options">
+          {
+            options.map((option, index) => {
+              return (
+                <div key={index}>
+                  <label htmlFor={option.label}>
+                    {option.label}
+                  </label>
+                  <input
+                    type="checkbox"
+                    value={option.value}
+                    id={option.label}
+                    onChange={(e) => {handleEvent(e, option.value, title)}}>
+                  </input>
+                </div>
+              )
+            })
+          }
+      </div>
+    </div>
+  )};
 
 const FilterOptions = () => filterOptions.map((option, index) => <FilterOption key={index} {...option} />);
 
