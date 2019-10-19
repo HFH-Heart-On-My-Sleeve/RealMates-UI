e0a39d7ddb301509c14740f818b4a8d3f9f4f7f8
Edeline Amelia <15990238+edeline@users.noreply.github.com>
2019-10-19 23:41:22 +1100

    Basic next step wizard form thing

--- src/components/help-seeker-form/index.js
+++ src/components/help-seeker-form/index.js
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import FormQuestionLayout from '../form-question-layout';
 import { FilterGrid } from '../filter-options';
 
+import './index.scss';
+
 const HelpSeekerForm = () => {
-  return (
-    <React.Fragment>
-      <h2>Who would you like to talk to?</h2>
-      <FormQuestionLayout title="Someone with experience with...">
-        <FilterGrid />
-      </FormQuestionLayout>
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formSelection, setFormSelection] = useState({step1: [], step2: [], step3: []});
 
-      <FormQuestionLayout title="I am going to ask you another question?">
-        <FilterGrid />
-      </FormQuestionLayout>
+  switch (currentStep) {
+    case 1:
+      return (
+        <React.Fragment>
+          <h2>Who would you like to talk to?</h2>
+          <FormQuestionLayout title="Someone with experience with...">
+            <FilterGrid />
+          </FormQuestionLayout>
 
-      <FormQuestionLayout title="MOAR QUESTIONS!!!!">
-        <FilterGrid />
-      </FormQuestionLayout>
-    </React.Fragment>
-  );
+          <FormQuestionLayout title="I am going to ask you another question?">
+            <FilterGrid />
+          </FormQuestionLayout>
+
+          <FormQuestionLayout title="MOAR QUESTIONS!!!!">
+            <FilterGrid />
+          </FormQuestionLayout>
+
+          <button
+            className="help-seeker-form__submit"
+            onClick={() => setCurrentStep(currentStep + 1)}>CONTINUE</button>
+        </React.Fragment>
+      );
+    default:
+      return (
+        <React.Fragment>
+          <h2>Other filters that might help you</h2>
+          <FormQuestionLayout title="Someone with experience with...">
+            <FilterGrid />
+          </FormQuestionLayout>
+
+          <FormQuestionLayout title="I am going to ask you another question?">
+            <FilterGrid />
+          </FormQuestionLayout>
+
+          <FormQuestionLayout title="MOAR QUESTIONS!!!!">
+            <FilterGrid />
+          </FormQuestionLayout>
+
+          <button
+            className="help-seeker-form__submit"
+            onClick={() => console.log(formSelection)}>SUBMIT</button>
+        </React.Fragment>
+      )
+  }
 };
 
 export default HelpSeekerForm;
