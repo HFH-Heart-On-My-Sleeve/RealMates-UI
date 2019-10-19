b0457b0a88be4b88f1efcf0ca84059d6c1bf33f9
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-20 09:00:22 +1100

    added age range and language json

---
+++ src/db/hardcoded-agerange.js
@@ -0,0 +1,18 @@
+export const agerange = [
+  {
+    start: 18,
+    end: 25
+  },
+  {
+    start: 25,
+    end: 35
+  },
+  {
+    start: 35,
+    end: 50
+  },
+  {
+    start: 65,
+    end: 120
+  }
+];
