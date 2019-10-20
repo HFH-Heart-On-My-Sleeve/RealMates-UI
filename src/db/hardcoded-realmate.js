0a7d4f7f5c8c5681e2a673bcf5e065c22c6d582f
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-10-20 11:37:49 +1100

    hardcoded-realmate added

---
+++ src/db/hardcoded-realmate.js
@@ -0,0 +1,172 @@
+export const realmates = [{
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FPN1K4WR4-56398e8a5f/20191019_165411.bw_720.jpg",
+    name: "Sachini",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: [
+      "25-35",
+      "F",
+      "No clinical diganosis",
+      "Upset, Confused, Uncertain, Sad",
+      "Loss or death",
+      "not applicable",
+      "Sri Lanka, Australia",
+      "Sinhalese",
+      "Postgraduate",
+      "Professional services",
+      "Buddhism"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FPN1K45JS-2c14e6fd80/20191019_165015.bw_720.jpg",
+    name: "Robert",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["65+",
+      "M",
+      "No clinical diganosis",
+      "Upset, Confused, Uncertain, Sad",
+      "Loss or death", "not applicable",
+      "Australia, Ireland",
+      "English",
+      "Degree",
+      "Retired",
+      "Judaism"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-pri/TP8SC75NJ-FPMLLLJ5V/mitch-01.bw.jpg",
+    name: "Matthew",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: [
+      "35-50",
+      "M",
+      "Anxiety Disorders",
+      "Abandoned, Upset, Disappointed, Enraged, Confused, Irritable",
+      "Family/Parenting, Relationship breakdown",
+      "not applicable",
+      "Australia",
+      "English",
+      "Degree",
+      "Managerial",
+      "Non-religious"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FPMLLLLMD-e385018091/20191019_165259.bw_720.jpg",
+    name: "Julia",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["25-35",
+      "F",
+      "Depressive Disorders",
+      "Enraged,Scared,Weak,Paranoid, Nervous,Uncertain",
+      "Relationship instability, domestic violence,  Sexual Abuse",
+      "not applicable",
+      "Australia",
+      "English",
+      "Degree",
+      "Sales",
+      "Non-religious"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FPKB5REH2-94c650e851/20191019_165328.bw_720.jpg",
+    name: "Michelle",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["35-50",
+      "F",
+      "Depressive Disorders",
+      "Upset, Confused, Uncertain",
+      "Family/Parenting, Loss or death",
+      "Rural",
+      "Australia",
+      "English",
+      "Certificate",
+      "Clerical and Administrative Workers",
+      "non-religious"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FP71TTF18-443766a78f/20191019_165055_0_.bw_720.jpg",
+    name: "Brenda",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["25-35",
+      "F",
+      "Depressive Disorders,Anxiety Disorders,Addiction",
+      "Overwhelmed,Distraught,Empty,Lonely,Exhausted",
+      "Insecure,Financial,Relationship instability",
+      "Rural",
+      "Australia",
+      "English",
+      "Tafe",
+      "Machinery Operators And Drivers",
+      "non-religious"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FPKR228MT-c01a1a61ce/20191019_165130.bw_720.jpg",
+    name: "Zhang",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["25-35",
+      "M",
+      "No clinical diganosis",
+      "Overwhelmed,Empty,Lonely,Exhausted,  Doubtful",
+      "Insecure, Life transition",
+      "Migrant",
+      "Chinese",
+      "Mandarin, English",
+      "Postgraduate",
+      "Professional services",
+      "Chinese traditional"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-pri/TP8SC75NJ-FPDBYC6Q1/20191019_165102.bw.jpg",
+    name: "Anna",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["25-35",
+      "F",
+      "Anxiety Disorders",
+      "Upset, Confused, Uncertain, Sad",
+      "LGBTI,Bullying, career",
+      "not applicable",
+      "Australia, Indonesia",
+      "English, Indonesian",
+      "Postgraduate",
+      "Student",
+      "Christianity"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-pri/TP8SC75NJ-FPMLLLJ5V/mitch-01.bw.jpg",
+    name: "Mark",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["50-65",
+      "M",
+      "Depressive Disorders, Personality Disorder",
+      "Disappointed, Confused,  Uncertain,  Guilty",
+      "Relationship instability,  Financial",
+      "not applicable",
+      "Australian",
+      "English",
+      "High school",
+      "Sales",
+      "non-religious"
+    ]
+  },
+  {
+    imgUrl: "https://files.slack.com/files-tmb/TP8SC75NJ-FP71TTE3C-4e68095c9e/20191019_165021.bw_720.jpg",
+    name: "Kiera",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus aliquam quam, non mollis risus suscipit vitae. Praesent sodales vulputate arcu sed fringilla. Curabitur a fringilla elit, quis faucibus enim. Proin ac nisl eu diam auctor tincidunt. Donec vel maximus erat, in convallis quam. Aenean auctor placerat ex nec vestibulum. Praesent convallis leo id faucibus scelerisque.",
+    filters: ["25-35",
+      "F",
+      "No clinical diganosis",
+      "Lost, Tense, Overwhelmed",
+      "career",
+      "Rural",
+      "Au",
+      "E",
+      "Certificate",
+      "Entertainment",
+      "non-religious"
+    ]
+  }
+];
