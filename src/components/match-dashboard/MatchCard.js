b6f3a699c3479ab1f506d2f7a568d33939d79fce
Edeline Amelia <15990238+edeline@users.noreply.github.com>
2019-10-20 12:48:48 +1100

    Only show 6 tags

--- src/components/match-dashboard/MatchCard.js
+++ src/components/match-dashboard/MatchCard.js
@@ -1,22 +1,31 @@
 import React from 'react';
 
-const MatchCard = ({ imgUrl, name, description, filters }) => (
-  <div className="match-dashbboard__match">
-    <div className="match-dashbboard__match-avatar">
-      <img
+const MatchCard = ({ imgUrl, name, description, filters }) => {
+  const filtersToShow = filters.slice(0, 6);
+  console.log("filtersToShow", filtersToShow);
+  const restOfFiltersCount = filters.length - 5;
+
+  return (
+    <div className="match-dashbboard__match">
+      <div>
+        <div className="match-dashbboard__match-avatar">
+        <img
         className="match-dashbboard__match-image"
         src={imgUrl}
         alt={name}
-      />
+        />
+        </div>
+        <h2>{name}</h2>
+        <div className="match-dashbboard__match-desc">
+        { description }
+        </div>
+      </div>
+      <div className="match-dashbboard__match-filters">
+        { filtersToShow.map(filter => <div key={filter} className="match-dashbboard__match-filter">{filter}</div>) }
+        <p>+ { restOfFiltersCount } more</p>
+      </div>
     </div>
-    <h2>{name}</h2>
-    <div className="match-dashbboard__match-desc">
-      { description }
-    </div>
-    <div className="match-dashbboard__match-filters">
-      { filters.map(filter => <div key={filter} className="match-dashbboard__match-filter">{filter}</div>) }
-    </div>
-  </div>
-);
+  );
+}
 
 export default MatchCard;
