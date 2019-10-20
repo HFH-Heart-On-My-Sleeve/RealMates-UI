df5371a169f71ecd7760b9e6c5624d50692c6287
Edeline Amelia <15990238+edeline@users.noreply.github.com>
2019-10-20 11:55:03 +1100

    Fix hardcoded data

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,7 @@
+export { agerange } from './hardcoded-agerange';
 export { countries } from './hardcoded-country';
 export { feelings } from './hardcoded-feeling';
+export { languages } from './hardcoded-language';
 export { mentalIllnesses } from './hardcoded-mental-ill';
 export { situations } from './hardcoded-situations';
+export { realmates } from './hardcoded-realmate';
